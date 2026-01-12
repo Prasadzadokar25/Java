3e90fa2c79c84cdd199f4c224c7151e285f0486d
prasadzadokar25 <prasadzadokar25@gmail.com>
2026-01-12 22:25:30 +0530

    Learning servlet day 12

---
+++ Daily_Codes/28_servlet/servletapp2/src/main/java/pdevelopment/MyServlet.java
@@ -0,0 +1,15 @@
+package pdevelopment;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+
+    @Override
+    public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.getWriter().println("My first templet");
+    }
+}
