81a6738be15a1b40ea7ac9a236a09cc7010fc0b3
prasadzadokar25 <prasadzadokar25@gmail.com>
2026-01-18 22:00:03 +0530

    learning java advance day 17

--- Daily_Codes/28_servlet/servletapp2/src/main/java/pdevelopment/ReadingRequestParameter.java
+++ Daily_Codes/28_servlet/servletapp2/src/main/java/pdevelopment/ReadingRequestParameter.java
@@ -1,8 +1,6 @@
 package pdevelopment;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 public class ReadingRequestParameter {
 
